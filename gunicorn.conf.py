e7ef4292f1b58cb7239929e27e3b117c7c4f22ff
NEXUS\bri246 <robert.bridgart@csiro.au>
2024-11-14 10:12:32 +1100

    Update gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,15 @@
+import gevent.monkey
+gevent.monkey.patch_all()
+
 from prometheus_flask_exporter.multiprocess import \
     GunicornInternalPrometheusMetrics
 
+# Settings, https://docs.gunicorn.org/en/stable/settings.html
+# Check what the server sees: gunicorn --print-config datacube_wps:app
+timeout = 600   # 10 mins
+worker_class = 'gevent'
+workers = get_pod_vcpus() * 2 + 1
+reload = True
 
 def child_exit(server, worker):
     GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
